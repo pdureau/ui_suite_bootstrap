e820079d49ed905c30753a9e1451e55bd9957cca
Pierre <pierredureau@yahoo.fr>
2019-09-22 19:36:10 +0200

    Init with bootstrap_barrio/scripts/create_subtheme.sh

---
+++ js/global.js
@@ -0,0 +1,16 @@
+/**
+ * @file
+ * Global utilities.
+ *
+ */
+(function ($, Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.ui_patterns_bootstrap = {
+    attach: function (context, settings) {
+
+    }
+  };
+
+})(jQuery, Drupal);
