f27fb96a571da1ca9e045bb33cd17b57acf85407
florenttorregrosa <florenttorregrosa@2388214.no-reply.drupal.org>
2022-06-28 17:30:33 +0200

    Issue #3292595 by Grimreaper: Bootstrap 5 : Components > Navbar

--- src/HookHandler/PreprocessPatternNavbar.php
+++ src/HookHandler/PreprocessPatternNavbar.php
@@ -5,11 +5,20 @@ declare(strict_types = 1);
 namespace Drupal\ui_suite_bootstrap\HookHandler;
 
 use Drupal\Component\Utility\Html;
+use Drupal\Core\Render\Element;
 
 /**
  * Handle variables needed for the pattern.
  */
 class PreprocessPatternNavbar {
+
+  /**
+   * Pattern fields where render elements will be injected.
+   */
+  public const REGIONS_FIELDS = [
+    'navigation',
+    'navigation_collapsible',
+  ];
 
   /**
    * Handle variables needed for the pattern.
@@ -19,6 +28,36 @@ class PreprocessPatternNavbar {
    */
   public function preprocess(array &$variables): void {
     $variables['navbar_id'] = Html::getUniqueId('bootstrap-navbar');
+
+    if (substr($variables['variant'], 0, 4) === 'dark') {
+      foreach (self::REGIONS_FIELDS as $region) {
+        if (isset($variables[$region])) {
+          $this->addDarkSetting($variables[$region]);
+        }
+      }
+    }
+  }
+
+  /**
+   * Add dark setting to navbar_nav pattern.
+   *
+   * @param array $item
+   *   The render element to parse.
+   */
+  protected function addDarkSetting(array &$item): void {
+    if (isset($item['#type']) &&
+      in_array($item['#type'], ['pattern', 'pattern_preview']) &&
+      $item['#id'] == 'navbar_nav'
+    ) {
+      $item['#dark'] = TRUE;
+      // Stop recursion when a navbar_nav is found, no need to enter in it.
+      return;
+    }
+
+    $children = Element::children($item);
+    foreach ($children as $key) {
+      $this->addDarkSetting($item[$key]);
+    }
   }
 
 }
---
+++ src/HookHandler/PreprocessPatternNavbarNav.php
@@ -0,0 +1,83 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Drupal\ui_suite_bootstrap\HookHandler;
+
+use Drupal\Core\Url;
+
+/**
+ * Handle variables needed for the pattern.
+ */
+class PreprocessPatternNavbarNav {
+
+  /**
+   * Handle variables needed for the pattern.
+   *
+   * @param array $variables
+   *   The preprocessed variables.
+   */
+  public function preprocess(array &$variables): void {
+    foreach ($variables['items'] as &$item) {
+      if (!($item['url'] instanceof Url)) {
+        $item['url'] = Url::fromUri($item['url']);
+      }
+
+      // Add 'nav-link' class.
+      $item['url']->mergeOptions(['attributes' => [
+        'class' => [
+          'nav-link',
+        ],
+      ]]);
+
+      // Add 'dropdown-item' patterns to below links.
+      if (isset($item['below']) && !empty($item['below'])) {
+        foreach ($item['below'] as &$sub_level_item) {
+          if (!($sub_level_item['url'] instanceof Url)) {
+            $sub_level_item['url'] = Url::fromUri($sub_level_item['url']);
+          }
+
+          if ($sub_level_item['url']->isRouted() && $sub_level_item['url']->getRouteName() === '<nolink>') {
+            $sub_level_item_attributes = $sub_level_item['url']->getOption('attributes');
+            // Divider.
+            if (isset($sub_level_item_attributes['class']) && in_array('dropdown-divider', $sub_level_item_attributes['class'])) {
+              $sub_level_item = [
+                '#type' => 'pattern',
+                '#id' => 'dropdown_item',
+                '#variant' => 'dropdown_divider',
+              ];
+            }
+            // Header.
+            elseif (isset($sub_level_item_attributes['class']) && in_array('dropdown-header', $sub_level_item_attributes['class'])) {
+              $sub_level_item = [
+                '#type' => 'pattern',
+                '#id' => 'dropdown_item',
+                '#variant' => 'dropdown_header',
+                '#content' => $sub_level_item['title'],
+              ];
+            }
+            // Text only.
+            else {
+              $sub_level_item = [
+                '#type' => 'pattern',
+                '#id' => 'dropdown_item',
+                '#variant' => 'dropdown_item_text',
+                '#content' => $sub_level_item['title'],
+              ];
+            }
+          }
+          else {
+            $sub_level_item = [
+              '#type' => 'pattern',
+              '#id' => 'button',
+              '#variant' => 'dropdown_item',
+              '#label' => $sub_level_item['title'],
+              '#url' => $sub_level_item['url']->toString(),
+            ];
+          }
+        }
+      }
+    }
+  }
+
+}
