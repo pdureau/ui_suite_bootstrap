7abd1f81463874f57198ed7d78888f738317f024
Florent Torregrosa <14238-florenttorregrosa@users.noreply.drupalcode.org>
2022-10-30 16:53:36 +0100

    Issue #3292614 by Grimreaper: Bootstrap 5 : Utilities > Interactions

---
+++ assets/js/accessibility.es6.js
@@ -0,0 +1,40 @@
+((Drupal, once) => {
+  Drupal.ui_suite_boostrap_accessibility =
+    Drupal.ui_suite_boostrap_accessibility || {};
+
+  Drupal.ui_suite_boostrap_accessibility.pointer_elements = ["a", "input"];
+
+  function preventClick(element) {
+    if (
+      Drupal.ui_suite_boostrap_accessibility.pointer_elements.includes(
+        element.tagName.toLowerCase()
+      )
+    ) {
+      element.setAttribute("tabindex", "-1");
+      element.setAttribute("aria-disabled", "true");
+    } else {
+      const children = Array.from(element.children);
+      children.forEach((child) => {
+        // Do not prevent click in the case of a clickable element inside a
+        // non-clickable container.
+        if (!child.classList.contains("pe-auto")) {
+          preventClick(child);
+        }
+      });
+    }
+  }
+
+  Drupal.behaviors.ui_suite_boostrap_accessibility = {
+    attach(context) {
+      // https://getbootstrap.com/docs/5.2/utilities/interactions/#pointer-events.
+      // Add aria-disabled and tabindex on clickable elements with the "pe-none"
+      // class.
+      const unclickableElements = once(
+        "unclickable-element",
+        ".pe-none",
+        context
+      );
+      unclickableElements.forEach(preventClick);
+    },
+  };
+})(Drupal, once);
